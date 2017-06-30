98a4aac936b264cbe163ddd203be7cbe6231edd2
lramosouza <lramosouza@outlook.com>
2017-06-29 23:04:02 -0300

    saque

--- src/com/telegrambotbank/opcoes/helper/DepositoBancarioHelper.java
+++ src/com/telegrambotbank/opcoes/helper/DepositoBancarioHelper.java
@@ -83,7 +83,7 @@ public class DepositoBancarioHelper {
 		return dadosOperacaoCredito;
 	}
 
-	public static String solicitarNuContaDestinoDeposito(TelegramBot bot, Update update, BigDecimal valorDeposito)
+	public static String solicitarNuContaDestinoDeposito(TelegramBot bot, Update update)
 			throws SaldoInsuficienteException, ContaOuAgenciaInvalidaException, IOException, CampoInvalidoException {
 
 		GetUpdatesResponse updatesResponse;
@@ -226,7 +226,74 @@ public class DepositoBancarioHelper {
 		if (!arquivoContaCorrenteReaderCredito.isArquivoExistente(arquivoCredito)) {
 			throw new ContaOuAgenciaInvalidaException();
 		}
+	}
 
+	public static String solicitarContaCliente(TelegramBot bot, Update update) throws CampoInvalidoException {
+		GetUpdatesResponse updatesResponse;
+
+		boolean fimInformarConta = false;
+
+		int m;
+
+		String resp = StringUtilsEnum.BLANK.getBlank();
+
+		bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+		bot.execute(new SendMessage(update.message().chat().id(),
+				"Favor Informe o número da sua conta"));
+		m = update.updateId() + 1;
+
+		while (fimInformarConta == false) {
+			updatesResponse = bot.execute(new GetUpdates().limit(100).offset(m + 2));
+
+			// lista de mensagens
+			List<Update> updates4 = updatesResponse.updates();
+
+			// análise de cada ação da mensagem
+			for (Update update3 : updates4) {
+				resp = update3.message().text().trim();
+				if (resp != null) {
+					Utils.validarTamanhoMensagem(resp, PosicoesCamposEnum.NU_CONTA_CORRENTE.getPosicaoMin(),
+							PosicoesCamposEnum.NU_CONTA_CORRENTE.getPosicaoMax());
+					fimInformarConta = true;
+				}
+			}
+		}
+		return resp;
+	}
+
+	public static String solicitarAgenciaCliente(TelegramBot bot, Update update) throws CampoInvalidoException {
+
+		GetUpdatesResponse updatesResponse;
+
+		boolean fimInformarAgencia = false;
+
+		int m;
+
+		bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+		bot.execute(new SendMessage(update.message().chat().id(),
+				"Favor Informe a Agência da sua conta"));
+
+		m = update.updateId() + 1;
+
+		String resp = StringUtilsEnum.BLANK.getBlank();
+
+		while (fimInformarAgencia == false) {
+			updatesResponse = bot.execute(new GetUpdates().limit(100).offset(m + 1));
+
+			// lista de mensagens
+			List<Update> updates3 = updatesResponse.updates();
+
+			// análise de cada ação da mensagem
+			for (Update update2 : updates3) {
+				resp = update2.message().text().trim();
+				if (resp != null) {
+					Utils.validarTamanhoMensagem(resp, PosicoesCamposEnum.AGENCIA_CONTA_CORRENTE.getPosicaoMin(),
+							PosicoesCamposEnum.AGENCIA_CONTA_CORRENTE.getPosicaoMax());
+					fimInformarAgencia = true;
+				}
+			}
+		}
+		return resp;
 	}
 
 }
